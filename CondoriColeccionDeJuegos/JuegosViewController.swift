013feb3cc6a68080278926161572404c3c6914ef
sesentayseis <carlos.condori.c@tecsup.edu.pe>
2023-05-17 14:39:16 -0500

    TERCER COMMIT

--- CondoriColeccionDeJuegos/JuegosViewController.swift
+++ CondoriColeccionDeJuegos/JuegosViewController.swift
@@ -12,11 +12,24 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var JuegoImageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
     
+    @IBOutlet weak var eliminarBoton: UIButton!
+    
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    
     var imagePicker = UIImagePickerController()
+    var juego:Juego? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        
+        if juego != nil {
+           JuegoImageView.image = UIImage(data: (juego!.imagen!) as Data)
+           tituloTextField.text = juego!.titulo
+           agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+       }else{
+           eliminarBoton.isHidden = true
+       }
     }
     
     
@@ -29,15 +42,28 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func agregarTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let juego = Juego(context:context)
-        juego.titulo = tituloTextField.text
-        juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
-        (UIApplication.shared.delegate as! AppDelegate).saveContext()
-        navigationController?.popViewController(animated: true)
+        if juego != nil{
+                    juego!.titulo! = tituloTextField.text!
+                    juego!.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+                }else{
+                    let context = (UIApplication.shared.delegate as!
+                                   AppDelegate).persistentContainer.viewContext
+                    let juego = Juego(context: context)
+                    juego.titulo = tituloTextField.text
+                    juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+                }
+                
+                (UIApplication.shared.delegate  as! AppDelegate).saveContext()
+                navigationController?.popViewController(animated: true)
     }
     
     
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                context.delete(juego!)
+                (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                navigationController?.popViewController(animated: true)
+    }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let imagenSelecionada = info[.originalImage] as? UIImage
         JuegoImageView.image = imagenSelecionada
--- CondoriColeccionDeJuegos/ViewController.swift
+++ CondoriColeccionDeJuegos/ViewController.swift
@@ -36,6 +36,13 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
            
        }
     }
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let juego = juegos[indexPath.row]
+            performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
 }
 
